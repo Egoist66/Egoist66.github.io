7618eb3b5ccb03d6783e15ad70f4792a0af3573f
Farid <razormad666@gmail.com>
2022-01-07 21:25:28 +0300

    newadds

--- homework_8/js/script.js
+++ homework_8/js/script.js
@@ -5,3 +5,24 @@ $(document).ready(function () {
         list.toggleClass('move');
     })
 });
+
+
+
+
+var regButton = $('.EnterClub');
+
+// regButton.attr('name', 'newforma');
+
+// console.log(regButton.attr('name'));
+
+regButton.click(function (e) {
+    switch (confirm('Вы подтверждаете?')) {
+        case true:
+            alert('Вы зарегистрированы');
+            break;
+        default:
+            alert('Отмена регистрации');
+            e.preventDefault();
+
+    }
+})
