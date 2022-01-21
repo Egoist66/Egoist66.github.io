5021c5e9c91c741be352bb38fedb9dff365ad6ca
Farid <razormad666@gmail.com>
2022-01-21 22:36:57 +0300

    mysite

--- KostDiagnostics/js/script.js
+++ KostDiagnostics/js/script.js
@@ -2,6 +2,39 @@
 var section__bg = $('.third__section-bg');
 
 
+var elemArr = [$('#minusblock-title'), $('#plusblock-title')];
+
+
+var hiddenContent1 = $('.hiddenContent1');
+var hiddenContent2 = $('.hiddenContent2');
+var links = document.querySelectorAll('.header__topnavigationlist-links');
+
+links.forEach(link => {
+    link.addEventListener('click', function (event) {
+        event.preventDefault();
+    })
+})
+
+
+
+
+
+elemArr[0].click(function (event) {
+    event.preventDefault();
+    elemArr[0].toggleClass('active');
+    hiddenContent1.slideToggle(400);
+
+})
+
+elemArr[1].click(function (event) {
+    event.preventDefault();
+    elemArr[1].toggleClass('active');
+    hiddenContent2.slideToggle(400);
+
+})
+
+
+
 
 $(document).ready(function () {
     section__bg.toggleClass('active');
