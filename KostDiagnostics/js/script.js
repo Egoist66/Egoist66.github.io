aa62f09759776fc32dc423731c48755436ea2efe
Farid <razormad666@gmail.com>
2022-01-22 20:04:41 +0300

    motors

--- KostDiagnostics/js/script.js
+++ KostDiagnostics/js/script.js
@@ -1,41 +1,70 @@
 
 var section__bg = $('.third__section-bg');
-
-
+var arrow = document.querySelector('.isShownBtn');
+var arrowspan = document.querySelector('.arrowspan');
 var elemArr = [$('#minusblock-title'), $('#plusblock-title')];
-
-
 var hiddenContent1 = $('.hiddenContent1');
 var hiddenContent2 = $('.hiddenContent2');
 var links = document.querySelectorAll('.header__topnavigationlist-links');
 
-links.forEach(link => {
-    link.addEventListener('click', function (event) {
-        event.preventDefault();
+
+
+
+arrowspan.addEventListener('click', function (event) {
+    event.preventDefault();
+    const blockId = arrowspan.getAttribute('href');
+    document.querySelector('' + blockId).scrollIntoView({
+        behavior: 'smooth',
+        block: 'start'
     })
 })
 
 
+window.onscroll = () => {
+    if (window.scrollY > 400) {
+        arrow.classList.add('isShownBtn_hide');
 
-
-
-elemArr[0].click(function (event) {
-    event.preventDefault();
-    elemArr[0].toggleClass('active');
-    hiddenContent1.slideToggle(400);
-
-})
-
-elemArr[1].click(function (event) {
-    event.preventDefault();
-    elemArr[1].toggleClass('active');
-    hiddenContent2.slideToggle(400);
-
-})
+    } else {
+        arrow.classList.remove('isShownBtn_hide');
+    }
+};
 
 
 
 
 $(document).ready(function () {
+
+
+
     section__bg.toggleClass('active');
+
+    links.forEach(link => {
+        link.addEventListener('click', function (event) {
+            event.preventDefault();
+        })
+    })
+
+
+
+
+
+    elemArr[0].click(function (event) {
+        event.preventDefault();
+        elemArr[0].toggleClass('active');
+        hiddenContent1.slideToggle(400);
+
+    })
+
+    elemArr[1].click(function (event) {
+        event.preventDefault();
+        elemArr[1].toggleClass('active');
+        hiddenContent2.slideToggle(400);
+
+    })
+
+
+
+
+
+
 });
