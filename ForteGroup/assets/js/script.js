fcd5b49e64cd1d30431401b2923734190c421807
Farid <razormad666@gmail.com>
2022-03-22 18:08:15 +0300

    fix 3 forte

--- ForteGroup/assets/js/script.js
+++ ForteGroup/assets/js/script.js
@@ -44,9 +44,6 @@ let navigation = document.querySelector('.header-navigation');
 menu_burger.classList.add('menu-burger');
 menu_img.classList.add('menu-burger_img');
 menu_img.src = 'assets/images/burger.svg';
-
-
-
 menu_burger.append(menu_img);
 navigation.append(menu_burger);
 
@@ -54,10 +51,94 @@ navigation.append(menu_burger);
 
 
 
+// sticky header
+
+let header_box = document.querySelector('.header-box');
+let header = document.querySelector('.header');
+
+
+window.addEventListener('scroll', (event) => {
+
+    event.preventDefault();
+
+    if (scrollY > 0) {
+        header.classList.add('sticky');
+
+    } else {
+        header.classList.remove('sticky');
+
+
+    }
+
+    console.log(window.pageYOffset);
+
+})
+
+
+// active burger and menu
+
+let mobile_menu = document.querySelector('.mobile-menu');
+let mobile_navigation = document.querySelector('.header-navigation__list');
+mobile_menu.insertAdjacentHTML('afterbegin', `
+
+    <ul class="mobile-navigation__list">
+
+    <li>
+        <a href="#" class="mobile-navigation__list-links">Are we a fit?</a>
+    </li>
+    <li>
+        <a href="#" class="mobile-navigation__list-links">Technology expertise </a>
+    </li>
+    <li>
+        <a href="#" class="mobile-navigation__list-links">Insights </a>
+    </li>
+    <li>
+        <a href="#" class="mobile-navigation__list-links"> Our work</a>
+    </li>
+    <li>
+        <a href="#" class="mobile-navigation__list-links">About us</a>
+    </li>
+    <li>
+        <a href="#" class="mobile-navigation__list-links ">Careers</a>
+    </li>
+
+    <li> <button class="header-navigation__list-button">Get Started</button></li>
+
+    </ul>`
+)
+console.log(mobile_menu);
+
+
+function activeBurger() {
+    this.classList.toggle('active');
+    mobile_menu.classList.toggle('active');
+    mobile_navigation.classList.toggle('active');
+
+
+    if (mobile_menu.classList.contains('active')) {
+        document.documentElement.style.overflow = 'hidden';
+    } else {
+        document.documentElement.style.overflow = 'auto';
+    }
+}
+
+
+
+menu_burger.addEventListener('click', activeBurger);
 
 
 
 
+
+
+
+
+
+
+
+
+
+//*************
 // Jquery Module
 
 let isShownBtn = $('.isShownBtn')
@@ -78,6 +159,13 @@ $(window).scroll(function () {
 
 });
 
-// map_frame.hover(function () {
-//     map_frame.toggleClass('active');
+// $(window).scroll(function () {
+//     if ($(this).scrollTop() > 0) {
+//         $('.header-box').addClass('sticky');
+//         $('.header').css('margin-bottom', '0px');
+//     } else {
+//         $('.header-box').removeClass('sticky');
+//         $('.header').css('margin-bottom', '151px')
+//     }
 // });
+
